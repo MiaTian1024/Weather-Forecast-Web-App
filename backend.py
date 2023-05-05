76d25199bba02e80093618f78f1910a541d1394d
MiaTian1024 <sddxtm123@gmail.com>
2023-05-06 11:51:39 +1200

    Filtering forecast data

--- backend.py
+++ backend.py
@@ -6,8 +6,17 @@ def get_data(place, forecast_days, kind):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    match kind:
+        case "Temperature":
+            filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+        case "Sky":
+            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+
+    return filtered_data
 
 
 if __name__ == "__main__":
-    get_data()
+    print(get_data(place="Tokyo", forecast_days=3, kind="Sky"))
