762576b2dee123ae54c212bbe91439f9c176947f
MiaTian1024 <sddxtm123@gmail.com>
2023-05-06 12:23:01 +1200

    Add sky conditions

--- backend.py
+++ backend.py
@@ -2,21 +2,15 @@ import requests
 from config import API_KEY
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    match kind:
-        case "Temperature":
-            filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-        case "Sky":
-            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Sky"))
+    print(get_data(place="Tokyo", forecast_days=3))
