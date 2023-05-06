e8f6c70515291d8a6619a4765455ce4de8d21a1d
MiaTian1024 <sddxtm123@gmail.com>
2023-05-06 13:46:28 +1200

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,16 +1,15 @@
 import requests
-from config import API_KEY
-import streamlit as st
+# from config import API_KEY
 import os
 
 
 def get_data(place, forecast_days):
-    if API_KEY:
-        url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-    else:
-        API_KEY_ENV = os.environ["API_KEY"]
-        url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY_ENV}"
-        
+    # if API_KEY:
+        # url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    # else:
+    API_KEY_ENV = os.environ["API_KEY"]
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY_ENV}"
+
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
